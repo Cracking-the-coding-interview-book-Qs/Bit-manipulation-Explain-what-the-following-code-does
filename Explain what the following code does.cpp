770e8f4151c83f0d65caef8cc86c8e21b2d8338f
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-06-20 15:33:11 +0530

    Add files via upload

---
+++ Explain what the following code does.cpp
@@ -0,0 +1,15 @@
+//Q --> ((n & (n-1))==0)
+
+//Answer:
+/*
+Lets take examples
+4=0100
+3=0011
+
+here ((n & (n-1))==0) holds
+
+similarly it holds for all powers of 2
+
+
+So, The above code checks if n is a power of 2 or not.
+*/
